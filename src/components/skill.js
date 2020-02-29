acf71bdc320d6b21fd20d281c3f20df3156f7f3d
Antonia <antonia.krohn8@gmail.com>
2020-02-29 19:28:01 +0100

    Adjust skill component

--- src/components/skill.js
+++ src/components/skill.js
@@ -2,11 +2,16 @@ import React from "react"
 import Chip from "../components/chip"
 
 const Skill = ({ headline, content }) => (
-  <div className="">
+  <div className="py-12">
     <h3 className="text-xl text-sb-orange text-center mb-6 mt-8">{headline}</h3>
-    {content.map(item => (
-      <Chip key={item} content={item} />
-    ))}
+    <div className="h-32 w-100 flex justify-center items-center mb-6">
+      <div className="rounded-full h-20 w-20 bg-yellow-400"></div>
+    </div>
+    <div className="py-5">
+      {content.map(item => (
+        <Chip key={item} content={item} />
+      ))}
+    </div>
   </div>
 )
 
--- src/icons/Github.js
+++ src/icons/Github.js
@@ -3,7 +3,7 @@ import React from "react"
 const Github = () => (
   <span>
     <svg
-      enable-background="new 0 0 24 24"
+      enableBackground="new 0 0 24 24"
       viewBox="0 0 24 24"
       xmlns="http://www.w3.org/2000/svg"
     >
